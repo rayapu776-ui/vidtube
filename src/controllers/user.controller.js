79ae7b1454bdb8dfc611dc48e46a8030eb1fcd5a
rayapu776-ui <rayapu776@gmail.com>
2026-09-23 23:40:52 +0530

    login controller in MERN backend

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,6 +6,22 @@ import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import {ApiResponse} from "../utils/ApiResponse.js"
 
 
+const generateAccessAndRefereshToken = async (userId) => {
+ try {
+  const user = await User.findById(userId)
+ 
+  //Small check for user existence
+  const accessToken = user.generateAccessToken()
+  const refreshToken = user.generateRefreshToken()
+ 
+  user.refreshToken  =  refreshToken
+  await user.save({validateBeforeSave : false})
+  return {accessToken , refreshToken}
+ } catch (error) {
+  throw new ApiError(500, "Something went wrong while generating acces and refresh tokens")
+ }
+}
+
 const registerUser =  asyncHandler(async (req , res) => {
     const {fullanme , email , username , password } = reg.body
 
@@ -92,7 +108,53 @@ throw new ApiError(500 , "Failed to upload coverImage")
   }
 })
 
+const loginUser = asyncHandler (async (req , res)=> {
+  //get data from body 
+
+  const {email , use , password} = req.body
+
+  //validation
+  if(!email){
+    throw new ApiError(400 , "Email is required")
+  }
+
+  const user = await User.findOne({
+    $or : [{username} , {email}]
+  })
+ if(!user){
+  throw new ApiError(404 ,"User not found")
+ }
+
+ //validate password
+
+ const isPasswordValid =  await user.isPasswordCorrect(password)
+
+if(!isPasswordValid){
+  throw new ApiError(401 , "Invalid Credentials")
+}
+const {accessToken , refreshToken} = await generateAccessAndRefereshToken(user._id)
+
+
+const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+
+const options = {
+  httpOnly : true ,
+  secure : process.env.NODE_ENV === "production"
+}
+
+return res.status(200)
+.cookie("accessToken", accessToken , options)
+.cookie("refreshToken", refreshToken, options)
+.json(new ApiResponse(200 , loggedInUser ,
+  {user : loggedInUser , accessToken , refres},
+"User looged in succesffuly"))
+
+
+})
+
 
 export {
     registerUser,
+    loginUser,
+    
 }
